53f094f75d34e4f3e27cf915378395e9cd068167
Jason Hadi <jason.h@ecobee.com>
2026-03-20 10:31:41 -0400

    first agent update

--- agent.py
+++ agent.py
@@ -1,11 +1,14 @@
 from strands import Agent
 from strands.models.gemini import GeminiModel
+from strands_tools import current_time, file_write, python_repl
 
 model = GeminiModel(
     client_args={"api_key": ""},
     model_id="gemini-2.5-flash",
 )
 
-agent = Agent(model=model)
+agent = Agent(model=model, tools=[current_time, file_write, python_repl])
 
-agent("Hello from Toronto! How are you?")
+agent(
+    "Write a python file that calculates 5 iterations of the fibbonaci sequence and save it to a local file. Execute the python file."
+)
